2bc59c064a80b8eacea8a885b0d9da83a8090b4e
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-10-15 18:57:03 -0500

    removed commented code

--- crud/cargo/cargo_footer.php
+++ crud/cargo/cargo_footer.php
@@ -5,30 +5,3 @@
         $('.ui.dropdown').dropdown();
     </script>
 </html>
-
-<!-- <div class="ui container">
-        <form action="#" class="ui form">
-        <h2>Welcome to my Project</h2>
-            <div class="four fields">
-                <div class="field">
-                    <label for="">Name:</label>
-                    <input type="text">
-                </div>
-                <div class="field">
-                    <label for="">Name:</label>
-                    <input type="text">
-                </div>
-                <div class="field">
-                    <label for="">Name:</label>
-                    <input type="text">
-                </div>
-                <div class="field">
-                    <label for="">Name:</label>
-                    <input type="text">
-                </div>
-                    
-                </div>
-                <button class="ui basic button"><i class="play icon"></i> Save</button>
-            </div>
-        </form>
-    </div> -->
